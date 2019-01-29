65ff92499818f6d634f4117a73aca19e68fe8e43
NathanBlack262 <43217968+NathanBlack262@users.noreply.github.com>
2019-01-29 14:11:31 -0600

    Final Upload 2:11PM 1/29/2019
    Bubble Sort Started

--- sorting.py
+++ sorting.py
@@ -4,8 +4,24 @@ def insertion_sort():
     pass
 
 
-def bubble_sort():
-    pass
+def bubble_sort(arr,debug=False):
+    whole_row = False
+    while !whole_row:
+        whole_row_counter = 0
+        for i in range(0,len(arr)):
+            a = arr[i]
+            b = arr[i+1]
+            if a >= b:
+                arr[i] = b
+                arr[i+1] = b
+            elif b < a:
+                arr[i] = a
+                arr[i+1] = b
+            else:
+                whole_row_counter += 1
+                if (whole_row_counter == len(arr) - 1):
+                    
+                
     
     
 def selection_sort():
